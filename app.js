4e979cb25d0f9f79924d5500bb0c53ff4979fae2
hdennen <hdennen@users.noreply.github.com>
2016-04-16 14:18:16 +0200

    game board init

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-var nicknames = [];
-var game = new Game();
+var request = require('request');
+var nicknames = []; //to do: set a limit on this somewhere.
+var game = new Game(); //to do: dynamically instantiate this
+var gameRunning = false;
+var gameBoard = 'gameBoard'; //use socket.io's rooms functionality
+
 
 function Game(){ //game object
 	this.player1 = '';
@@ -13,24 +17,46 @@ function Game(){ //game object
 	this.p1score = 0;
 	this.p2score = 0;
 	this.movies = [['Casablanca',1942],['Whiplash',2014],['Memento',2000]]; //test data before live calls
+	//to do: populate game.movies from http://api.myapifilms.com/imdb/top?start=1&end=250&token=9cdf9b51-79af-4eeb-95ad-e50f3acaaf4e&format=json&data=0
 }
 
-function createGame(){ //create blank game
-	game = new Game();
+function getMovies(){
+	app.get('http://api.myapifilms.com/imdb/top?start=1&end=250&token=9cdf9b51-79af-4eeb-95ad-e50f3acaaf4e&format=json&data=0', function(req, res){
+		request(function(err, body){
+			if (err)
+				res.send(err)
+			var topMovies = JSON.parse(body);
+			//res.render('', locals);
+		});
+	});
 }
 
-function addPlayer1(player){
+function startGame(){ //run the game
+	//by this point the game has players and the gameBoard room is populated with the players.
+	gameRunning = true;
+	io.sockets.emit('new message', {msg: game.player1 + " and " + game.player2 + " started a game.", nick: 'robot'});
+	io.sockets.in(gameBoard).emit('game data', game);
+	io.sockets.in(gameBoard).emit('game message', {msg:"The game is afoot!", alert: 'alert-success'});
+}
+
+function addPlayer1(socket, player){
 	game.player1 = player //passed from socket.nickname
+	socket.join(gameBoard);
+	socket.emit('show board');
+	socket.emit('game message', {msg:"You're in!", alert: 'alert-success'});
 }
-function addPlayer2(player){
+function addPlayer2(socket, player){
 	game.player2 = player //passed from socket.nickname
+	socket.join(gameBoard);
+	socket.emit('show board');
+	socket.emit('game message', {msg:"You're in!", alert: 'alert-success'});
 }
 
 
 server.listen(3000);
-app.use(express.static(__dirname + '/public')); //lemme not deal with individual GET reqs...
+app.use(express.static(__dirname + '/public')); //lemme not deal with individual GET reqs... sorry angular.
 
-//turn on connection event, what happens when user sends something. kind of like document.ready
+//turn on connection event, what happens when user sends something. similar to document.ready
 io.sockets.on('connection', function(socket){ //function with user's socket
 	socket.on('new user', function(data, callback){
 		if(nicknames.indexOf(data)!= -1){ //check if exists
@@ -54,19 +80,36 @@ io.sockets.on('connection', function(socket){ //function with user's socket
 
 
 	//game controls ==========================
+
+	//join game-------------------
 	socket.on('join request', function(){ //add players to game
-		if(game.player1 != '' && game.player2 != ''){
+		if(game.player1 === socket.nickname || game.player2 === socket.nickname){ //check player's not already in game
+			//io.sockets.emit('new message', {msg: socket.nickname+", you're already in the game!", nick: 'robot'});
+			socket.emit('alert', {msg:"You're already in the game dummy!", alert: 'alert-danger'});
+		}else if(game.player1 != '' && game.player2 != ''){ //check game's not full
 			io.sockets.emit('new message', {msg: socket.nickname+', the game is full dude.', nick: 'robot'});
-			console.log("game full");
-		}else if(game.player1 === ''){
-			addPlayer1(socket.nickname);
-			io.sockets.emit('new message', {msg: socket.nickname+' created a new game!', nick: 'robot'});
+		}else if(game.player1 === ''){ //add as player 1
+			addPlayer1(socket, socket.nickname);
+			io.sockets.emit('new message', {msg: socket.nickname+' joined the game!', nick: 'robot'});
 			console.log(game);
-		}else if(game.player1 != '' && game.player2 === ''){
-			addPlayer2(socket.nickname);
+		}else if(game.player1 != '' && game.player2 === ''){ //add as player 2
+			addPlayer2(socket, socket.nickname);
 			io.sockets.emit('new message', {msg: socket.nickname+' joined the game!', nick: 'robot'});
 			console.log(game);
 		}
+		if(gameRunning === false && game.player1 != '' && game.player2 != ''){
+			startGame();
+		}
+	});
+	//maybe do: change emit to specific socket on already joined.
+
+	//game setup -------------------------
+
+
+
+	//answers--------------------------
+	socket.on('send answer', function(){
+
 	});
 
 
@@ -75,5 +118,16 @@ io.sockets.on('connection', function(socket){ //function with user's socket
 		if(!socket.nickname) return; //if leaving before setting nickname
 		nicknames.splice(nicknames.indexOf(socket.nickname), 1); //splice user from array
 		updateNicks();
+		if(socket.nickname === game.player1){
+			game.player1 = '';
+			gameRunning = false;
+			io.sockets.emit('new message', {msg: socket.nickname+'aborted game and left the arcade.', nick: 'robot'});
+			console.log(game);
+		}else if(socket.nickname === game.player2){
+			game.player2 = '';
+			gameRunning = false;
+			io.sockets.emit('new message', {msg: socket.nickname+' aborted game and left the arcade.', nick: 'robot'});
+			console.log(game);
+		}
 	});
 });
--- public/js/front.js
+++ public/js/front.js
@@ -10,12 +10,21 @@ jQuery(function($){
 	var $messageBox = $('#message');
 	var $chat = $('#chat');
 	//game vars
+	var $alerts = $('#alerts');
 	var $join = $('#join-request');
+	var $playWrap = $('#playWrap');
+	var $gameNotes = $('#gameNotes');
+	var $movie = $('#movieStuff');
 	var $sendAnswer = $('#send-answer');
 	var $answer = $('#answer');
 
+	var	$p1 = $('#p1nick');
+	var	$p2 = $('#p2nick');
+	var	$p1s = $('#p1score');
+	var	$p2s = $('#p2score');
 
 
+ //nicknames controls==============================
 	$nickForm.submit(function(e){
 		e.preventDefault();
 		socket.emit('new user', $nickBox.val(), function(data){
@@ -44,16 +53,45 @@ jQuery(function($){
 		socket.emit('send message', $messageBox.val());
 		$messageBox.val('');
 	});
-	//recieve message
+	//receive message
 	socket.on('new message', function(data){
-		$chat.append('<b>'+ data.nick + ': </b>' + data.msg + "</br>");
+		if(data.nick === 'robot'){
+			$chat.append('<p class="alert alert-info"><b>'+ data.nick + ': </b>' + data.msg + "</p>");
+		}else{
+			$chat.append('<b>'+ data.nick + ': </b>' + data.msg + "</br>");
+		}
+		
 	});
+	//to do: do some check scroll magic on new message.
+
+
 
 	//game controls=====================================
-	$join.submit(function(e){
+	$join.submit(function(e){ //join
 		e.preventDefault();
 		socket.emit('join request'); //it has the nickname data
 	});
+	$sendAnswer.submit(function(e){ //answer
+		e.preventDefault();
+		socket.emit('send answer'); //it has the nickname data
+	});
 
+	socket.on('alert', function(data){
+		$alerts.html(data.msg).removeClass().addClass('alert '+data.alert).show().delay(1000).fadeOut("slow");
+	});
+
+ 	socket.on('show board', function(){
+		$playWrap.show();
+	});
+	socket.on('game message', function(data){
+		$gameNotes.removeClass().html(data.msg).addClass('alert '+data.alert).show().delay(1000).fadeOut("slow");
+	});
+	socket.on('game data', function(data){ //gets whole object at first. to do: later manipulate object on client side before sending back for top scores.
+		$p1.html(data.player1);
+		$p2.html(data.player2);
+		$p1s.html(data.p1score);
+		$p2s.html(data.p2score);
+		$movie.html(data.movies[0][0]);
+	});	
 
 });
